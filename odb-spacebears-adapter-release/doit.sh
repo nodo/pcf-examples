6030033d88474561ab35fd28649b6d55fef29f68
Andrea Nodari <anodari@pivotal.io>
2018-06-07 09:29:10 +0100

    WIP

--- odb-spacebears-adapter-release/doit.sh
+++ odb-spacebears-adapter-release/doit.sh
@@ -8,6 +8,16 @@ if [[ $# != 1 ]]; then
 fi
 
 environment="$1"
+
+## Build adapter source code and upload jar
+javac src/github.com/pcf-examples/odb-spacebears-adapter/Main.java
+jar -f /tmp/adapter.jar -c src/github.com/pcf-examples/odb-spacebears-adapter/*class
+bosh add-blob /tmp/adapter.jar adapter/adapter.jar
+bosh -e "$environment" upload-blobs
+
+## Create adapter release
 bosh -e "$environment" create-release --force --name dummy-adapter
 bosh -e "$environment" upload-release --name dummy-adapter
+
+## Deploy ODB
 bosh -e "$environment" -d spacebears-broker deploy -l secrets.yml example_manifests/spacebears-manifest.yml
---
+++ odb-spacebears-adapter-release/src/github.com/pcf-examples/odb-spacebears-adapter/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+  public static void main(String []args) {
+    ODBAdapter a = new ODBAdapter(args);
+  }
+}
+
+class ODBAdapter {
+  ODBAdapter(String []args) {
+    System.out.println("yolo");
+  }
+}
