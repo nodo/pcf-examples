0d425cc10e00a6e9be36c0f36307b40c5ab2b702
Andrea Nodari <anodari@pivotal.io>
2018-06-08 09:15:51 +0100

    WIP

--- odb-spacebears-adapter-release/src/github.com/pcf-examples/odb-spacebears-adapter/Main.java
+++ odb-spacebears-adapter-release/src/github.com/pcf-examples/odb-spacebears-adapter/Main.java
@@ -1,12 +1,72 @@
 
 public class Main {
   public static void main(String []args) {
-    ODBAdapter a = new ODBAdapter(args);
+    if (args == null || args.length < 1) {
+      System.err.printf("Wrong arguments!");
+      System.exit(1);
+    }
+    switch (args[0]) {
+      case "generate-manifest": new ManifestGenerator(args).run();
+      case "dashboard-url": new DashboardURL(args).run();
+      case "create-binding": new Binder(args).create();
+      case "delete-binding": new Binder(args).delete();
+      case "generate-plan-schemas": new SchemasGenerator(args).run();
+    }
   }
 }
 
-class ODBAdapter {
-  ODBAdapter(String []args) {
-    System.out.println("yolo");
+class ManifestGenerator {
+  String []args;
+
+  ManifestGenerator(String []args) {
+    this.args = args;
+  }
+
+  static void run() {
+    System.out.println("manifest generator run");
+  }
+}
+
+class DashboardURL {
+  String []args;
+
+  DashboardURL(String []args) {
+    this.args = args;
+  }
+
+  static void run() {
+    System.out.println("dashboard-url run");
+    System.exit(10); // not implemented
+  }
+}
+
+class Binder {
+  String []args;
+
+  Binder(String []args) {
+    this.args = args;
+  }
+
+  static void create() {
+    System.out.println("create-binding run");
+    System.exit(1); // Error!
+  }
+
+  static void delete() {
+    System.out.println("delete-binding run");
+    System.exit(1); // Error!
+  }
+}
+
+class SchemasGenerator {
+  String []args;
+
+  SchemasGenerator(String []args) {
+    this.args = args;
+  }
+
+  static void run() {
+    System.err.println("plan schemas generator run");
+    System.exit(10); // not implemented
   }
 }
